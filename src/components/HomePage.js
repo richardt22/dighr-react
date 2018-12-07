cd8e851b766b6a5ad7f93300f23ba863ab6f9ebb
richardt22 <richardtrinh22@gmail.com>
2018-12-07 11:20:47 -0500

    update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => (
   <div>
     <h1>Dahdaleh Institute for Global Health Research</h1>
-    <h2>Committed to a pursuit of Global Health that seeks healthy people, healthy communities, and a healthy planet.</h2>
+    <h2>Committed to a pursuit of Global Health that seeks healthy people, healthy communities, and a healthy planet. Test</h2>
   </div>
 );
 

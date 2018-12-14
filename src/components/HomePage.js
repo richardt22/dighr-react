0ab5b0209cef9660ea027469bd0b437ece5a2f5d
richardt22 <richardtrinh22@gmail.com>
2018-12-14 09:23:43 -0500

    update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => (
   <div className="homepage">
     <h2>Committed to a pursuit of Global Health that seeks healthy people, healthy communities, and a healthy planet.</h2>
     <h3>Explore Our Research Themes</h3>
-    <ul>
-        <li><a href="/planetaryhealth">Planetary Health</a></li>
-        <li><a href="/globalhealthhuman">Global Health Humanitarianism</a></li>
-        <li><a href="/globalhealthforesight">Global Health Forecasting</a></li>
-    </ul>
+    
+    <div className="row">
+        <div className="col">
+            <Link to="/planetaryhealth">Planetary Health</Link>
+        </div>
+
+        <div className="col">
+            <Link to="/globalhealthhuman">Global Health Humanitarianism</Link>
+        </div>
+
+        <div className="col">
+            <Link to="/globalhealthforesight">Global Health Forecasting</Link>
+        </div>
+    </div>
     
   </div>
 );
